3640fd0de11eb502e8bf6efd7d9dd096c44e042d
Marinofull <Marinofull@users.noreply.github.com>
2016-06-22 19:25:11 -0300

    agora o off-canvas pode ser fechado clicando na página

--- assets/js/rewrite.js
+++ assets/js/rewrite.js
@@ -1,16 +1,24 @@
 var toggleMenu  = document.querySelectorAll('.toggle-menu'),
     wrapper     = document.querySelector('.wrapper');
+    innerContent= document.querySelector('.inner-content');
 
 for(var i = 0; i < toggleMenu.length; i++){
     toggleMenu[i].addEventListener('click', menuAction);
     //window.addEventListener('click', menuAction);
 }
 
+innerContent.addEventListener('click', closeMenu);
+
 function menuAction() {
     if(wrapper.classList.contains('show-menu'))
         wrapper.classList.remove('show-menu');
     else
         wrapper.classList.add('show-menu');
+}
+
+function closeMenu() {
+    if(wrapper.classList.contains('show-menu'))
+        wrapper.classList.remove('show-menu');
 }
 
 document.addEventListener('keyup', function(e){
